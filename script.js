9575c0cfd4df96b62158a38f106e364652aa4cbc
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-04-19 10:07:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+let yourArray = [
+	{
+		one: "This",
+		two: 2,
+		three: true
+	},
+	{
+		four: "is",
+		five: 5,
+		six: false
+	},
+	{
+		seven: "the",
+		eight: 8,
+		nine: false
+	},
+	{
+		ten: "array",
+		eleven: 11,
+		twovel: true
+	},
+	{
+		thirteen: "function",
+		fourteen: 14,
+		fifyteen: true
+	}
+];
